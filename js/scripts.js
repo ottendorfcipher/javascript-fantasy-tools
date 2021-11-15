e1e1c2b21630328aa87120c1cf6b9c68344b1f5d
Nick Weiner <nicholas.weiner@icloud.com>
2021-11-15 15:11:37 -0500

    converting form to json

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,56 @@
 
+// Form Convert to JSON
+
+const formElement = document.querySelector('form')
+const getFormJSON = (form) => {
+    const data = new FormData(form);
+    return Array.from(data.keys()).reduce((result, key) => {
+      result[key] = data.get(key);
+      return result;
+    }, {});
+  };
+
+  const handler = (event) => {
+    event.preventDefault();
+    const valid = formElement.reportValidity();
+    if (valid) {
+      const result = getFormJSON(formElement);
+      console.log(result)
+    }
+  }
+
+  formElement.addEventListener("submit", handler)
+
 // NPC Color Options
 let colorOptions = ["1","2","3","4","5"];
 
+// Hair Style
+   
+const hairStyle = document.getElementById("hairStyle");
+let hairStyleOptions = ["1", "2", "3", "4", "5"];
+
+for(let i = 0; i < hairStyleOptions.length; i++) {
+let opt = hairStyleOptions[i];
+const el = document.createElement("option");
+el.textContent = opt;
+el.value = opt;
+hairStyle.appendChild(el);
+}
+
+// Complexion
+
+const complexion = document.getElementById("complexion");
+let complexionOptions = ["1", "2", "3", "4", "5"];
+
+for(let i = 0; i < complexionOptions.length; i++) {
+let opt = complexionOptions[i];
+const el = document.createElement("option");
+el.textContent = opt;
+el.value = opt;
+complexion.appendChild(el);
+}
+
+// colorOptions
 
 // Favorite Color One
    
@@ -29,32 +78,6 @@ let colorOptions = ["1","2","3","4","5"];
    favColorTwo.appendChild(el);
 }
    
-// Hair Style
-   
-   const hairStyle = document.getElementById("hairStyle");
-   let hairStyleOptions = ["1", "2", "3", "4", "5"];
-
-   for(let i = 0; i < hairStyleOptions.length; i++) {
-   let opt = hairStyleOptions[i];
-   const el = document.createElement("option");
-   el.textContent = opt;
-   el.value = opt;
-   hairStyle.appendChild(el);
-}
-
-// Complexion
-   
-   const complexion = document.getElementById("complexion");
-   let complexionOptions = ["1", "2", "3", "4", "5"];
-
-   for(let i = 0; i < complexionOptions.length; i++) {
-   let opt = complexionOptions[i];
-   const el = document.createElement("option");
-   el.textContent = opt;
-   el.value = opt;
-   complexion.appendChild(el);
-}
-
 // Eye Color
 
 const eyeColor = document.getElementById("eyeColor");
