e2cecd85094890861d1d703971050fb9f3c4e90a
Nick Weiner <nicholas.weiner@icloud.com>
2021-11-15 13:43:20 -0500

    array to populate npc form

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,82 @@
-/*!
-* Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
-*/
+
+// NPC Color Options
+let colorOptions = ["1","2","3","4","5"];
+
+
+// Favorite Color One
+   
+    const favColorOne = document.getElementById("favColorOne");
+    let favColorOneOptions = colorOptions;
+ 
+    for(let i = 0; i < favColorOneOptions.length; i++) {
+    let opt = favColorOneOptions[i];
+    const el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    favColorOne.appendChild(el);
+ }
+ 
+ // Favorite Color Two
+   
+   const favColorTwo = document.getElementById("favColorTwo");
+   let favColorTwoOptions = colorOptions;
+
+   for(let i = 0; i < favColorTwoOptions.length; i++) {
+   let opt = favColorTwoOptions[i];
+   const el = document.createElement("option");
+   el.textContent = opt;
+   el.value = opt;
+   favColorTwo.appendChild(el);
+}
+   
+// Hair Style
+   
+   const hairStyle = document.getElementById("hairStyle");
+   let hairStyleOptions = ["1", "2", "3", "4", "5"];
+
+   for(let i = 0; i < hairStyleOptions.length; i++) {
+   let opt = hairStyleOptions[i];
+   const el = document.createElement("option");
+   el.textContent = opt;
+   el.value = opt;
+   hairStyle.appendChild(el);
+}
+
+// Complexion
+   
+   const complexion = document.getElementById("complexion");
+   let complexionOptions = ["1", "2", "3", "4", "5"];
+
+   for(let i = 0; i < complexionOptions.length; i++) {
+   let opt = complexionOptions[i];
+   const el = document.createElement("option");
+   el.textContent = opt;
+   el.value = opt;
+   complexion.appendChild(el);
+}
+
+// Eye Color
+
+const eyeColor = document.getElementById("eyeColor");
+let eyeColorOptions = colorOptions;
+
+for(let i = 0; i < eyeColorOptions.length; i++) {
+let opt = eyeColorOptions[i];
+const el = document.createElement("option");
+el.textContent = opt;
+el.value = opt;
+eyeColor.appendChild(el);
+}
+
+// Hair Color
+
+const hairColor = document.getElementById("hairColor");
+let hairColorOptions = colorOptions;
+
+for(let i = 0; i < hairColorOptions.length; i++) {
+let opt = hairColorOptions[i];
+const el = document.createElement("option");
+el.textContent = opt;
+el.value = opt;
+hairColor.appendChild(el);
+}
